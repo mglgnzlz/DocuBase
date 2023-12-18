42e71e44f0b5e63e7d0e20ee30a67488c9320b94
Miguel Gonzalez <mglgnzlz13@gmail.com>
2023-12-18 17:46:58 +0800

    [add]Basic Document Layout implemented, for further improvements

--- my_venv/main.py
+++ my_venv/main.py
@@ -1,36 +1,71 @@
 from tkinter import *
- 
+from tkinter import ttk, filedialog
+from PIL import Image, ImageTk
+
+def browseImage():
+    filename = filedialog.askopenfilename()
+    if filename:
+        loadAndDisplayImage(filename)
+
+def loadAndDisplayImage(filename):
+    image = Image.open(filename)
+    image = image.resize((400, 400), resample=Image.LANCZOS)  # Use Lanczos resampling
+    photo = ImageTk.PhotoImage(image)
+    imageLabel.config(image=photo)
+    imageLabel.image = photo
+
 root = Tk()
-root.geometry("300x300")
-root.title(" Camera Scanner ")
- 
-doc = Label(text = "Document name:",
-            anchor = W ,
-            justify= LEFT)
-doctxt = Text(root, height = 2,
-                width = 35,
-                bg = "light yellow"
-            )
+root.title("Document Form")
 
-date = Label(text = "Date: ")
-datetxt = Text(root, height = 2,
-                width = 35,
-                bg = "light yellow")
- 
-fm = Frame(root, width=300, height=200, bg="blue")
-fm.pack(side=BOTTOM, expand=NO, fill=NONE)
-    
-Button(fm, text="retake", width=10).pack(side=LEFT)
-Button(fm, text="add", width=10).pack(side=LEFT)
-Button(fm, text="finish", width=10).pack(side=LEFT)
+# Frame for the entire content
+contentFrame = ttk.Frame(root, padding=(10, 10, 10, 10))
+contentFrame.grid(column=0, row=0, sticky=(N, S, E, W))
 
-fm.place(x= 20, y= 20)
-    
- 
-doc.pack()
-doctxt.pack()
-date.pack()
-datetxt.pack()
-fm.pack()
- 
-mainloop()
+# Frame for image display
+imageFrame = ttk.Frame(contentFrame, borderwidth=2, relief="solid")  # Add border
+imageFrame.grid(column=0, row=0, rowspan=5, padx=10, sticky=(N, S, W, E))
+
+# Image box on the left side
+imageLabel = ttk.Label(imageFrame, text="Image Preview", width=40, anchor='center')
+imageLabel.grid(column=0, row=0, sticky=(N, S, W, E))  # No rowspan needed
+
+# Frame for input fields
+inputFrame = ttk.Frame(contentFrame, borderwidth=2, relief="solid")  # Add border
+inputFrame.grid(column=1, row=0, sticky=(N, S, W, E))
+
+# Labels and Entry fields on the right side
+documentNameLabel = ttk.Label(inputFrame, text="Document Name:")
+documentNameLabel.grid(column=0, row=0, sticky=W, padx=10, pady=(0, 2))  # Adjust pady
+
+documentNameEntry = ttk.Entry(inputFrame)
+documentNameEntry.grid(column=1, row=0, sticky=(W, E), padx=10, pady=(0, 2))  # Adjust pady
+
+dateLabel = ttk.Label(inputFrame, text="Date:")
+dateLabel.grid(column=0, row=1, sticky=W, padx=10, pady=(0, 2))  # Adjust pady
+
+dateEntry = ttk.Entry(inputFrame)
+dateEntry.grid(column=1, row=1, sticky=(W, E), padx=10, pady=(0, 2))  # Adjust pady
+
+# Button to browse for an image
+browseButton = ttk.Button(inputFrame, text="Browse Image", command=browseImage)
+browseButton.grid(column=0, row=2, columnspan=2, pady=10, sticky=(W, E))  # Adjust row number and columnspan
+
+# Column and Row configurations for resizing
+contentFrame.columnconfigure(0, weight=0)  # Allow the first column (image frame) to resize
+contentFrame.columnconfigure(1, weight=1)
+contentFrame.rowconfigure(0, weight=1)
+contentFrame.rowconfigure(5, weight=1)  # Adjust row number
+
+imageFrame.columnconfigure(0, weight=1)  # Allow the image frame to resize
+imageFrame.rowconfigure(0, weight=1)
+
+inputFrame.columnconfigure(0, weight=1)  # Allow the input frame to resize
+inputFrame.columnconfigure(1, weight=1)
+inputFrame.rowconfigure(0, weight=0)
+inputFrame.rowconfigure(1, weight=0)
+inputFrame.rowconfigure(2, weight=0)
+
+root.columnconfigure(0, weight=1)  # Allow the entire window to resize
+root.rowconfigure(0, weight=1)
+
+root.mainloop()
